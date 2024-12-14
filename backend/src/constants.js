808fc55b17e8b0359a1512b0a214108ab4a732be
mThanuj <mullagurithanuj0@gmail.com>
2024-12-14 18:25:23 +0530

    basic models

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,7 +1,8 @@
 import { config } from "dotenv";
 config({
-  path: "../.env",
+  path: "./.env",
 });
 
-export const MONGODB_URI = process.env.MONGODB_URI;
+const DB_NAME = "todos";
+export const MONGODB_URI = process.env.MONGODB_URI.replace("~", DB_NAME);
 export const PORT = process.env.PORT || 3000;
---
+++ backend/src/models/todo.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+const Todo = mongoose.models("Todo") || mongoose.model("Todo", todoSchema);
+
+export default Todo;
